759dd6699eb7c5f8bdbe9964bf5e320ba4b0d073
The Vrapi <101073476+thevrapi@users.noreply.github.com>
2025-08-02 00:48:14 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+// app.use(cors())
 app.use((req, res, next) => {
     console.log(`INCOMING ${req.method} in path: ${req.path}`)
     next()
